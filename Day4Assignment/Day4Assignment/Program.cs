f0996231ddfcff7b0f3e3b2dca60ff9546eb540f
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-07-27 15:51:03 +0530

    [Ankita] Add : UC1-count a total number of duplicate elements

---
+++ Day4Assignment/Day4Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day4Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayCount arrayCount = new ArrayCount();
+            arrayCount.MethodArrayCount();
+            Console.ReadLine();
+        }
+    }
+}
